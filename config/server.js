060f0db6631861c26437a4e5c6d0b688ce5f5ca0
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-01-10 18:29:10 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6915144765284459439d73aa7683f811'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9baeb33c-58b0-40bd-ac37-033ffbfc2934'
+};
