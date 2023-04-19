917e794879a7eaf4765cfe4a71350ee33716a206
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-04-19 00:29:06 -0300

    feat: Return 404 when there are no games found

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -7,21 +7,28 @@
 
 module.exports = {
   createPaymentIntent: async (ctx) => {
-    const { cart } = ctx.request.body
+    const { cart } = ctx.request.body;
 
-    let games = []
+    let games = [];
 
     await Promise.all(
       cart?.map(async (game) => {
         const validatedGame = await strapi.services.game.findOne({
           id: game.id
-        })
+        });
 
         if (validatedGame) {
           games.push(validatedGame)
         }
       })
-    )
+    );
+
+    if (!games.length) {
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!",
+      };
+    }
 
     return games
   }
