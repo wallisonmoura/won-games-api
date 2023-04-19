236b433bd136b6aa0302aa7af8502ad16752353e
wallisonmoura <wallison_cedaspy@hotmail.com>
2023-04-19 00:23:47 -0300

    feat: Search Cart Game info

---
+++ api/order/controllers/order.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body
+
+    let games = []
+
+    await Promise.all(
+      cart?.map(async (game) => {
+        const validatedGame = await strapi.services.game.findOne({
+          id: game.id
+        })
+
+        if (validatedGame) {
+          games.push(validatedGame)
+        }
+      })
+    )
+
+    return games
+  }
+};
